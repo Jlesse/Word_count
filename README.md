b60df8f021cd3574a5a23c473ed1a1c6981925fd
Jlesse <jlesseagile@gmail.com>
2016-01-23 11:22:09 -0600

    edit file for description and add README

---
+++ README.md
@@ -0,0 +1,14 @@
+# WORD FREQUENCY COUNTER
+## TESTING INSTRUCTIONS
+1. Make sure ruby is installed.
+3. In the terminal, in the project root, run then command:
+  bundle install
+to install rspec (if it's not already installed).
+4. Run the command "rspec spec" from the project root to run all the tests in test_spec.rb
+
+##USE INTSTRUCTIONS
+1. Make sure ruby is installed
+2. In the terminal, in the project root, run the command:
+  ruby runner.rb ./path/to/file1 ./path/to/file2 etc...
+3. You may include as many files as you want to runner.rb but it may begin to affect performace with larger volumes...
+
--- Word_Count.rb
+++ Word_Count.rb
@@ -1,22 +1,24 @@
-# test1 = "Even at his job—he was a technician in an electronic lab—Edgar couldn't stop longing for that era when existence had been more gentle, simple and leisurely. His social life virtually ceased.
-# \"Man, you ain\'t livin\',\" said one of the technicians he worked with. \"We're gonna buzz a few dives tonight. Why not drag it along with us?\""
 
-# test2 = "some words, , , , , more"
-
-$test3 = "\“This much is already known: for every sensible line of straightforward statement, there are leagues of senseless cacophonies, verbal jumbles and incoherences. (I know of an uncouth region whose librarians repudiate the vain and superstitious custom of finding a meaning in books and equate it with that of finding a meaning in dreams or in the chaotic lines of one\'s palm . . . They admit that the inventors of this writing imitated the twenty-five natural symbols, but maintain that this application is accidental and that the books signify nothing in themselves. This dictum, we shall see, is not entirely fallacious.)\"
-― Jorge Luis Borges, The Library Of Babel"
-
-
-# A class containing a run method and utility methods to assist in reading in a text string and counting the number of occurances of each
-# word. All the methods are called inside the run method.
+# A class containing a run method and utility methods to assist in reading in a text
+# string and counting the number of occurrences of each word.
 class Word_Count
 
+  # holds all words found paired with their occurrences
   @@word_hash = {}
 
+
+  # setter for the @@word_hash
+  # PARAMS:
+  # - hash: a hash, usually for our purposes an emtpy one to clear @@word_hash for testing
+  # RETURNS
+  # - @@word_hash: holds all words found paired with their occurrences
   def self.word_hash=(hash)
     @@word_hash = hash
   end
 
+  # getter the @@word_hash
+  # RETURNS:
+  # - @@word_hash: holds all words found paired with their occurrences
   def self.word_hash
     @@word_hash
   end
@@ -30,12 +32,12 @@ class Word_Count
    text.scan(/[a-zA-Z\d]+/)
   end
 
-  # Counts the number of occurances of each word and adds the word with its count
+  # Counts the number of occurrences of each word and adds the word with its count
   # the word_hash
   # PARAMS:
   # - word_array: an array of word strings
   # RETURNS:
-  # - a hash where each key is a word and its value is the number of occurances of that word in the word_array (ex. { "words"=> 4, "are"=> 2, "cool"=> 5 })
+  # - a hash where each key is a word and its value is the number of occurrences of that word in the word_array (ex. { "words"=> 4, "are"=> 2, "cool"=> 5 })
   def self.count_words(word_array)
     word_array.each do |word|
       word.downcase!
@@ -50,10 +52,10 @@ class Word_Count
 
   # Converts hash to an array and sorts the and to be in descending order and
   # PARAMS:
-  # - hash: an unsorted hash of word keys with their occurance values
+  # - hash: an unsorted hash of word keys with their occurrence values
   # RETURNS:
   # - a descending by value array of sub-arrays, where each subarray[0] is the word
-  # key and subarray[1] is its occurance value.
+  # key and subarray[1] is its occurrence value.
   def self.sort_hash(hash)
     hash.sort{|word1, word2| word2[1] <=> word1[1]}
   end
@@ -61,11 +63,11 @@ class Word_Count
   # Slices the first ten elements from sorted_words_array into a new array and returns it.
   # PARAMS:
   # - sorted_words_array: an ordered array of subarrays where each subarray
-  # contains the word as its first element and its number of occurances as its
+  # contains the word as its first element and its number of occurrences as its
   # second element.
   # RETURNS:
   # - a descending by value array of 10 sub-arrays, where each subarray[0] is the
-  # word key and subarray[1] is its occurance value.
+  # word key and subarray[1] is its occurrence value.
   def self.top_ten(sorted_words_array)
     sorted_words_array[0..9]
   end
