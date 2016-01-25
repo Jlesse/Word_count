bdfa13c89b26381c7bc70312330f3481788dfba1
Jlesse <jlesseagile@gmail.com>
2016-01-24 22:19:17 -0600

    add read in all files in folder feature

--- File_Reader.rb
+++ File_Reader.rb
@@ -2,13 +2,22 @@
 # A class that reads in a plan text file
 class File_Reader
 
+  def clean_path(dir)
+    if !(/.*\/\z/.match dir)
+      dir+="/"
+    end
+    if (/\A\/.*/.match dir)
+      dir = "./" + dir
+    end
+    dir
+  end
+
   # reads a file path in from the command line, opens the file, and concats each line together into a string
   #PARAMS:
   # - no params, the file_path is supplied with ARGV from the command line.
   #RETURNS
   # a string of all the chars read in from the file
   def read_in_file(file_path)
-
     file_string = ""
     if (File.file?(file_path))
       File.open(file_path, "r") do |f|
--- runner.rb
+++ runner.rb
@@ -1,6 +1,7 @@
 require_relative 'File_Reader'
 require_relative 'Word_Count'
 require_relative 'Print_View'
+require_relative 'Parser'
 
 # A class that runs the fileReader, WordCount, and printView. It attempts to open
 # each filePath and read it into a single string (file_string). it then passes it
@@ -25,21 +26,27 @@ class Main
 
   puts "Calculating"
 
-  # read in each file path from ARGV
-  ARGV.each do |file_path|
+  #clean the path of the directory (check if prepended by ./ and appended with /)
+  directory = file_reader.clean_path(ARGV[0])
+
+  dir = Dir.new(directory)
+
+  dir.each do |file_path|
+
+    #skip over file_path if its the current of grand parent directory or not a .txt file
+    next if file_path == '.' || file_path == '..' || !(/.+\.txt/.match(file_path))
 
     # let the user know the program didin't crash if the file is long...
     puts "..."
 
     # read the file in from the file path and parse into a single string
-    file_string = file_reader.read_in_file(file_path)
+    file_string = file_reader.read_in_file(directory + file_path)
 
     # parse the string into an array of words
     word_array = parser.parse_text(file_string)
 
     # count the word occurances
     word_counter.count_words(word_array)
-
 
   end
 
@@ -48,5 +55,7 @@ class Main
 
   #1st ten words from the hash
   top_words = word_counter.top_ten
+
+  #print list to console
   viewer.print_list(top_words)
 end
